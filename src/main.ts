46e8d3c88017828775d11ccfbc59244e878b4d6a
Cokke <jorgerandomgamer@gmail.com>
2024-12-05 20:13:41 -0300

    questionnaires actualizacion env

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowedOrigins = process?.env?.FRONTEND_URLS?.split(',');
+
   app.useGlobalPipes(new ValidationPipe());
 
-   app.enableCors({
-    origin: 'http://localhost:8081',
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins?.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('No permitido por CORS'));
+      }
+    },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
